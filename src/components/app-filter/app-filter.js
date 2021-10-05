c06120b367179d9110db94ad33b08f51d55f3155
Sorini92 <artembedlinsky@gmail.com>
2021-10-05 18:44:06 +0300

    done search

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,25 +1,40 @@
+import { Component } from 'react';
 import './app-filter.css';
 
-const AppFilter = () => {
-    return (
-        <div className="btn-group">
-            <button 
-            className="btn btn-light"
-            type="button">
-                Все содрудники
-            </button>
-            <button 
-            className="btn btn-outline-light"
-            type="button">
-                На повышение
-            </button>
-            <button 
-            className="btn btn-outline-light"
-            type="button">
-                З/П больше 1000$
-            </button>
-        </div>
-    );
+class AppFilter extends Component{
+    constructor(props) {
+        super(props);
+        this.setState = {
+            data: this.props.data
+        }
+    }
+    render() {
+        return (
+            <div className="btn-group">
+                <button 
+                    className="btn btn-light"
+                    type="button"
+                    //onClick={data}
+                    >
+                    Все содрудники
+                </button>
+                <button 
+                    className="btn btn-outline-light"
+                    type="button"
+                    //onClick={() => filterEmpOnRise()}
+                    >
+                    На повышение
+                </button>
+                <button 
+                    className="btn btn-outline-light"
+                    type="button" 
+                    //onClick={() => filterEmpOnAbove()}
+                    >
+                    З/П больше 1000$
+                </button>
+            </div>
+        );
+    }
 }
 
 export default AppFilter;
